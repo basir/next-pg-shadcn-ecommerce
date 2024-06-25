f41e34dc88f969de11b46de10eef322ffd53a0cf
basir <basir.jafarzadeh@gmail>
2024-06-25 10:38:59 +0330

    33. email order receipt by Resend - fix

--- email/index.tsx
+++ email/index.tsx
@@ -6,12 +6,10 @@ import { Order } from '@/types'
 const resend = new Resend(process.env.RESEND_API_KEY as string)
 
 export const sendPurchaseReceipt = async ({ order }: { order: Order }) => {
-  console.log(order.user.email)
-  const res = await resend.emails.send({
+  await resend.emails.send({
     from: `${APP_NAME} <${SENDER_EMAIL}>`,
     to: order.user.email,
     subject: 'Order Confirmation',
     react: <PurchaseReceiptEmail order={order} />,
   })
-  console.log(res)
 }
